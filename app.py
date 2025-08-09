c1bb578c3a540d7f9fda56d3e1ab56ecd7ac3ccf
Chaitanya Agarwal <chaitanyaarush@gmail.com>
2025-08-09 21:55:45 +0530

    Update for deploying on Streamlit

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def get_lifestyle_suggestions():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_ENV") != "production"
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(port=port, debug=debug)
